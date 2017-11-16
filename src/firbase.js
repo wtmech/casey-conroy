f399383b19e5464da6ae119dc317f5784342d586
Billy Mech <billy@fanhero.com>
2017-11-16 13:56:42 -0500

    firebase.js

--- src/firbase.js
+++ src/firbase.js
@@ -1,13 +1,1 @@
-import firebase from 'firebase';
-
-const config = {
-  apiKey: "AIzaSyAZqE2dUSgPW0WUbhowF2TCvuOJ5cWhyy8",
-  authDomain: "casey-conroy.firebaseapp.com",
-  databaseURL: "https://casey-conroy.firebaseio.com",
-  projectId: "casey-conroy",
-  storageBucket: "casey-conroy.appspot.com",
-  messagingSenderId: "951851752833"
-};
-firebase.initializeApp(config);
-
-export default firebase;
+//
