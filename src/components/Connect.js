0ee03daba867de770bc857fc360bae623c13b69b
Billy Mech <wmech16@gmail.com>
2017-11-17 16:13:18 -0500

    formspree

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -15,8 +15,9 @@ class Connect extends Component {
         <Header />
         <div className="connect-content">
         <Form />
+        <div className="connect-img-contain">
         <img className="connect-img" src="https://firebasestorage.googleapis.com/v0/b/casey-conroy.appspot.com/o/connect.png?alt=media&token=8258514f-74c7-452b-a286-7b4f87516803" alt=""/>
-        
+        </div>
         </div>
         <Footer />
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -24,12 +24,11 @@ class Form extends Component {
     
     return (
       <div className="input-wrapper">
-        <form action={`mailto:wmech16@gmail.com?subject=${this.state.subject}&body=${this.state.message}`} method="post" enctype="text/plain">
+        <form action="https://formspree.io/wmech16@gmail.com" method="POST">
         <input className="name form" type="text" name="name" placeholder="name" /><br/>
-        <input className="email form" type="text" name="mail" placeholder="email" /><br/>
-        <input className="subject form" type="text" name="subject" value={this.state.subject} onChange={this.handleSubjectChange} size="50" placeholder="subject" /><br/><br/>
-        <textarea className="message form" placeholder="message" name="message" value={this.state.message} onChange={this.handleMessageChange} ></textarea>
-        <input type="submit" value="Send" className="send"/>
+        <input className="email form" type="text" name="email" placeholder="email" /><br/>
+        <textarea className="message form" placeholder="message" name="message" ></textarea>
+        <input type="submit" className="send"/>
         </form>
         
       </div>
